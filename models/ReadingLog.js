47bcde08a4eb99777f7ad2b72456f92e898cfd3c
523810 <damenke15@gmail.com>
2026-05-25 20:53:49 +0900

    독서 기록 수정

--- models/ReadingLog.js
+++ models/ReadingLog.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const readingLogSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
+  bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: false }, // 💡 책 선택 없이도 기록 가능
   date: { type: String, required: true }, // "2026-03-13" 처럼 날짜 글자로 저장 (그래프 그리기 편함)
   readPages: { type: Number, default: 0 } // 그날 몇 쪽 읽었는지
 });
